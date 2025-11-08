e84646583bcea301268ad73c8cdc88bea20908e3
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-11-08 13:13:05 +0530

    Exception handling fix with swagger

--- src/main/java/com/cloudbook/common/exception/ApiExceptionHandler.java
+++ src/main/java/com/cloudbook/common/exception/ApiExceptionHandler.java
@@ -1,13 +1,15 @@
 package com.cloudbook.common.exception;
 
 import com.cloudbook.common.exception.payload.ExceptionMessage;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//@RestControllerAdvice TODO: Uncomment this when exception handling is to be enabled globally
+@RestControllerAdvice
+@Hidden
 @Slf4j
 public class ApiExceptionHandler {
 
